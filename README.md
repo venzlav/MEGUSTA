4bea132699ed92b09ddba6a388049ad65ac86e02
GarothLongint <michalekkow@gmail.com>
2012-04-25 20:45:27 +0200

    rm some spam files

--- README.md
+++
@@ -1,4 +0,0 @@
-MEGUSTA
-=======
-
-Wyszukiwarka ftp
--- scaner.php
+++
@@ -1,5 +0,0 @@
-<?php
-//$result = exec("/usr/bin/python /var/www/scaner.py");
-$file = '/home/michal/files.txt';
-file_put_contents($file, "kipa");
-?>
